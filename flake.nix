fdeea2d38c8c123deaae9717afadc3f4d9ae7d58
Daniel Poelzleithner <git@poelzi.org>
2026-01-29 04:09:48 +0100

    feat(WP01/T001): add lark and jinja2 dependencies

--- flake.nix
+++ flake.nix
@@ -2,33 +2,55 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    spec-kitty = {
+      url = "github:poelzi/spec-kitty/nix-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      spec-kitty,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
         python = pkgs.python311;
 
         # Runtime dependencies
-        pythonDeps = ps: with ps; [
-          click
-          rich
-          sqlalchemy
-          tomli-w
-        ];
+        pythonDeps =
+          ps: with ps; [
+            click
+            jinja2
+            lark
+            rich
+            sqlalchemy
+            tomli-w
+          ];
 
         # Development dependencies
-        devDeps = ps: with ps; [
-          pytest
-          pytest-cov
-          mypy
-          ruff
-        ];
+        devDeps =
+          ps: with ps; [
+            pytest
+            pytest-cov
+            mypy
+            ruff
+          ];
 
+        # Python environment for music-commander (runtime deps only)
+        python-music-cmd = pkgs.writeShellScriptBin "python-music-cmd" ''
+          exec ${python.withPackages pythonDeps}/bin/python "$@"
+        '';
+
+        # Full dev environment with all deps
         pythonEnv = python.withPackages (ps: (pythonDeps ps) ++ (devDeps ps));
 
-      in {
+      in
+      {
         packages.default = python.pkgs.buildPythonApplication {
           pname = "music-commander";
           version = "0.1.0";
@@ -43,12 +65,20 @@
 
           # Skip tests during build (run via checks)
           doCheck = false;
+
+          # Install default config file
+          postInstall = ''
+            mkdir -p $out/share/music-commander
+            cp ${./config.example.toml} $out/share/music-commander/config.example.toml
+          '';
         };
 
         devShells.default = pkgs.mkShell {
           buildInputs = [
             pythonEnv
+            python-music-cmd
             pkgs.git-annex
+            spec-kitty.packages.${system}.default
           ];
 
           shellHook = ''
@@ -56,12 +86,16 @@
           '';
         };
 
-        checks.default = pkgs.runCommand "pytest" {
-          buildInputs = [ pythonEnv ];
-        } ''
-          cd ${./.}
-          pytest --tb=short
-          touch $out
-        '';
-      });
+        checks.default =
+          pkgs.runCommand "pytest"
+            {
+              buildInputs = [ pythonEnv ];
+            }
+            ''
+              cd ${./.}
+              pytest --tb=short
+              touch $out
+            '';
+      }
+    );
 }
